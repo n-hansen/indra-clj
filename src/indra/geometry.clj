f56e2c584ad3d8e45b30014550847f4ff53a03f5
Nick Hansen <n-hansen@users.noreply.github.com>
2018-09-23 00:19:03 -0700

    incomplete support for lines

--- src/indra/geometry.clj
+++ src/indra/geometry.clj
@@ -2,7 +2,8 @@
   (:require [indra.complex :as c]
             [indra.mobius :as m]
             [net.cgrand.xforms :as x])
-  (:import [org.apache.commons.math3.complex Complex]))
+  (:import [org.apache.commons.math3.complex Complex]
+           [org.apache.commons.math3.util Precision]))
 
 ;; various Transformable objects
 
@@ -31,15 +32,33 @@
                                        (conj q*))))))))
           points)))
 
+(declare ->Line)
+
 (defrecord Circle [^Complex center ^double radius]
   m/Transformable
   (m/transform [this {:keys [c d] :as t}]
-    (let [z (c/- center
-                 (-> (c/div d c)
-                     (c/+ center)
-                     c/conjugate
-                     c/reciprocal
-                     (c/*real (* radius radius))))
-          center* (m/transform z t)]
-      (->Circle center*
-                (c/abs (c/- center* (m/transform (c/+real center radius) t)))))))
+    (if ; we get a line iff |d/c+P|=r
+        (-> (c/div d c)
+            (c/+ center)
+            c/abs
+            (Precision/equals radius))
+      (let [[p1 p2] (into [] (comp (map #(-> % (c/+ center) (m/transform t))) ; TODO solve explicitly
+                                   (remove c/inf?))
+                          [(c/rect radius 0)
+                           (c/rect 0 radius)
+                           (c/rect (- radius) 0)])]
+        (->Line p1 (c/argument (c/- p2 p1))))
+      (let [z (c/- center
+                   (-> (c/div d c)
+                       (c/+ center)
+                       c/conjugate
+                       c/reciprocal
+                       (c/*real (* radius radius))))
+            center* (m/transform z t)]
+        (->Circle center*
+                  (c/abs (c/- center* (m/transform (c/+real center radius) t))))))))
+
+(defrecord Line [^Complex offset ^double direction]
+  m/Transformable
+  (m/transform [this t]
+    (throw (ex-info "line transformations not yet implemented" {}))))
