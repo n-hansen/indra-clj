75e06a821ee5d8edddb34a9ffe6be103c3c66445
Nick Hansen <n-hansen@users.noreply.github.com>
2018-09-22 21:21:06 -0700

    add some basic recipes

---
+++ src/indra/mobius/recipes.clj
@@ -0,0 +1,26 @@
+(ns indra.mobius.recipes
+  (:require [indra.complex :as c]
+            [indra.geometry :as g]
+            [indra.mobius :as m]))
+
+;; generating and checking properties of mobius transforms with some nice pre-determined recipes
+
+(defn pure-translation
+  "z ↦ z + b"
+  [b]
+  (m/->Transformation c/one b c/one c/zero))
+
+(defn pure-scaling
+  "z ↦ kz"
+  [k]
+  (let [sqrt-k (c/sqrt k)]
+    (m/->Transformation sqrt-k c/zero c/zero (c/reciprocal sqrt-k))))
+
+(defn real-line-mapped-to-itself?
+  "does the extended real line map to itself?"
+  [t]
+  (every? #(-> t % c/imag (= 0)) [:a :b :c :d]))
+
+(def cayley-map
+  "map the real axis to the unit circle"
+  (m/->Transformation c/one (c/- c/i) c/one c/i))
