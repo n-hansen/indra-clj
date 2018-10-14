d5b03fd0a0d321a04853b0f422e0771c65748c6b
Nick Hansen <n-hansen@users.noreply.github.com>
2018-10-14 00:32:05 -0700

    fixed depth word enumeration for fixed points

---
+++ src/indra/limit_sets.clj
@@ -0,0 +1,68 @@
+(ns indra.limit-sets
+  (:require [indra.mobius :as m]))
+
+;; DFS with static depth and fixed preimage list
+
+(defn word->transform
+  [a a* b b* word]
+  (->> word
+       (map (fn [ltr]
+              (case ltr
+                :a a
+                :A a*
+                :b b
+                :B b*)))
+       (reduce m/compose)))
+
+(defn inverse-letter?
+  [ltr1 ltr2]
+  (case ltr1
+    :a (= :A ltr2)
+    :A (= :a ltr2)
+    :b (= :B ltr2)
+    :B (= :b ltr2)))
+
+(defn next-letter
+  [ltr]
+  (case ltr
+    :a :b
+    :b :A
+    :A :B
+    :B :a))
+
+(defn prev-letter
+  [ltr]
+  (case ltr
+    :b :a
+    :A :b
+    :B :A
+    :a :B))
+
+(defn first-word
+  "first word of length n on the cayley graph"
+  [depth]
+  (loop [w [:a]]
+    (if (= depth (count w))
+      w
+      (recur (conj w (-> w peek prev-letter))))))
+
+(defn next-word
+  [word]
+  (let [final-ix (dec (count word))]
+    (loop [word word
+           ix final-ix]
+      (when-not (neg? ix)
+        (let [x (next-letter (nth word ix))]
+          (if (inverse-letter? x (nth word (dec ix) :A)) ; root node order is :a :b :A :B
+            ;; walk towards the cayley graph root
+            (recur word (dec ix))
+            ;; orient towards next child then descend
+            (loop [word (assoc word ix x)
+                   ix ix]
+              (if (= ix final-ix)
+                word
+                (recur (assoc word
+                              (inc ix)
+                              (-> word (nth ix) (prev-letter)))
+                       (inc ix))))))))))
+
---
+++ test/indra/limit_sets_test.clj
@@ -0,0 +1,34 @@
+(ns indra.limit-sets-test
+  (:require [clojure.test :as t :refer [deftest testing is]]
+            [indra.complex :as c]
+            [indra.limit-sets :refer :all]
+            [indra.mobius :as m]))
+
+(deftest letter-test
+  (testing "next-letter is cyclic"
+    (let [letters [:a :b :A :B]]
+      (is (every? #(-> % next-letter next-letter next-letter next-letter (= %))
+                  letters))))
+  (testing "letters are in the correct cyclic order"
+    (let [letters [:a :b :A :B]]
+      (is (every? #(-> % next-letter next-letter (inverse-letter? %))
+                  letters)))))
+
+(deftest next-word-test
+  (testing "increment"
+    (is (= (next-word [:a]) [:b]))
+    (is (= (next-word [:b]) [:A]))
+    (is (= (next-word [:A]) [:B]))
+    (is (= (next-word [:B]) nil))
+    (is (= (next-word [:a :a]) [:a :b])))
+  (testing "carry"
+    (is (= (next-word [:a :b]) [:b :a]))
+    (is (= (next-word [:a :b :A]) [:b :a :B]))
+    (is (= (next-word [:B :a]) nil)))
+  (testing "a long sequence"
+    (is (= (apply * 4 (repeat 9 3))
+           (->> (first-word 10)
+                (iterate next-word)
+                (take-while some?)
+                count))))
+  )
