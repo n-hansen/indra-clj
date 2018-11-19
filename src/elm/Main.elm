e4ce775f5de3554f73d98c08293cc3ab790f5c50
Nick Hansen <n-hansen@users.noreply.github.com>
2018-11-18 17:50:25 -0800

    grid view

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,58 +1,111 @@
 import Browser
 import Complex as C
 import Complex exposing (Complex)
-import Html exposing (Html, div, img, text)
-import Html.Attributes exposing (src)
-import Html.Events exposing (onClick)
+import Css exposing (displayFlex, flex, flexDirection)
+import Html.Styled exposing (Html, Attribute, toUnstyled, div, img, text)
+import Html.Styled.Attributes exposing (css, src)
+import Html.Styled.Events exposing (onClick)
 import List
+import List.Extra as List
+import Maybe
+import Regex
+import Round exposing (round)
 import String as Str
 import Url.Builder as Url
 
 -- Main
 
-main =
-    Browser.sandbox { init = init, update = update, view = view}
+main = Browser.sandbox { init = init, update = update, view = view >> toUnstyled}
 
 -- Model
 
 type alias Model =
+    { centeredAt : FractalParameters
+    }
+
+type alias FractalParameters =
     { a : Complex
     , b : Complex
     }
 
 init : Model
-init = { a = C.complex 1.89 0.09
-       , b = C.complex 1.89 -0.09
+init = { centeredAt = { a = C.complex 1.89 0.09
+                      , b = C.complex 1.89 -0.09
+                      }
        }
 
 -- Update
 
-type Msg = CenterAt Complex Complex
+type Msg = ChangeCenter FractalParameters
 
 update : Msg -> Model -> Model
-update (CenterAt a b) _ = { a = a, b = b }
+update (ChangeCenter params) m = { m | centeredAt = params }
 
 -- View
 
 view : Model -> Html Msg
-view model =
-    List.range -2 2
-        |> List.map
-              ( \offset ->
-                    let
-                        offsetA = toFloat offset |> (*) 0.01 |> C.imaginary |> C.add model.a
-                    in
-                        (renderedImage offsetA model.b)
-              )
-        |> div []
+view {centeredAt} =
+    div [ gridStyle ] (computeRows centeredAt)
 
-renderedImage : Complex -> Complex -> Html Msg
-renderedImage a b =
+computeRows : FractalParameters -> List (Html Msg)
+computeRows fp =
+    perturbHorizontal fp
+        |> List.map (computeColumns >> div [ rowStyle ])
+
+perturbHorizontal : FractalParameters -> List FractalParameters
+perturbHorizontal ({a} as fp) =
+    List.range -1 1
+        |> List.map (\d -> { fp | a = toFloat d |> (*) 0.01 |> C.real |> C.add a })
+
+computeColumns : FractalParameters -> List (Html Msg)
+computeColumns fp =
+    perturbVertical fp
+        |> List.map fractalViewport
+
+perturbVertical : FractalParameters -> List FractalParameters
+perturbVertical ({a} as fp) =
+    List.range -1 1
+        |> List.map (\d -> { fp | a = toFloat d |> (*) 0.01 |> C.imaginary |> C.add a })
+
+gridStyle : Attribute Msg
+gridStyle =
+    css [ displayFlex
+        , flexDirection Css.row
+        ]
+
+rowStyle : Attribute Msg
+rowStyle =
+    css [ displayFlex
+        , flex (Css.int 1)
+        , flexDirection Css.column
+        ]
+
+-- Fractal viewports
+
+complexRound : Int -> Complex -> String
+complexRound n z =
     let
-        params = [ a |> C.toCartesian >> .re >> Str.fromFloat >> Url.string "ax"
-                 , a |> C.toCartesian >> .im >> Str.fromFloat >> Url.string "ay"
-                 , b |> C.toCartesian >> .re >> Str.fromFloat >> Url.string "bx"
-                 , a |> C.toCartesian >> .im >> Str.fromFloat >> Url.string "by"
+        censorZero s =
+            let
+                regex = Maybe.withDefault Regex.never (Regex.fromString "^[0.]+i?$")
+            in
+                if Regex.contains regex s then "" else s
+        {re,im} = C.toCartesian z
+        reStr = round n re |> censorZero
+        imStr = round n im ++ "i" |> censorZero
+        joinStr = if (reStr /= "") && (imStr /= "")
+                  then " + " else ""
+    in
+        reStr ++ joinStr ++ imStr
+
+
+fractalViewport : FractalParameters -> Html Msg
+fractalViewport fp =
+    let
+        params = [ fp.a |> C.toCartesian >> .re >> Str.fromFloat >> Url.string "ax"
+                 , fp.a |> C.toCartesian >> .im >> Str.fromFloat >> Url.string "ay"
+                 , fp.b |> C.toCartesian >> .re >> Str.fromFloat >> Url.string "bx"
+                 , fp.a |> C.toCartesian >> .im >> Str.fromFloat >> Url.string "by"
                  , Url.int "size" 220
                  , Url.int "depth" 80
                  , Str.fromFloat 1.0 |> Url.string "epsilon"
@@ -60,6 +113,6 @@ renderedImage a b =
         sourceUrl = Url.crossOrigin "http://localhost:8420" ["render"] params
     in
         div []
-            [ img [ src sourceUrl, onClick (CenterAt a b) ] []
-            , div [] [text <| "a=" ++ C.toString a ++ ", b=" ++ C.toString b]
+            [ img [ src sourceUrl, onClick (ChangeCenter fp) ] []
+            , div [] [text <| "a=" ++ complexRound 2 fp.a ++ ", b=" ++ complexRound 2 fp.b]
             ]
